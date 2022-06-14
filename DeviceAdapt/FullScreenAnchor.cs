ddabef8d6153bb7760dc151b14182a5943167d5a
Rocker Luo <78175054@qq.com>
2022-06-14 21:01:25 +0800

    挂接到每个窗口的适配代码

---
+++ DeviceAdapt/FullScreenAnchor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Wod.Game.Evt;
+using Wod.ThirdParty.Util;
+
+public class FullScreenAnchor : MonoBehaviour
+{
+    RectTransform  trans;
+    void Start()
+    {
+        trans = GetComponent<RectTransform>();
+        OrigentationChange();
+        Messenger.AddListener(UILayerEvent.SCREEN_ORIGENTATION_CHANGE, OrigentationChange);
+    }
+
+    private void OnDestroy()
+    {
+        Messenger.RemoveListener(UILayerEvent.SCREEN_ORIGENTATION_CHANGE, OrigentationChange);
+    }
+
+    private void OrigentationChange()
+    {
+        trans.offsetMin = new Vector2(ScreenSetting.Instance.PixelOffset.x, 0.0f);
+        trans.offsetMax = new Vector2(ScreenSetting.Instance.PixelOffset.y,1.0f);
+    }
+}
